6835f6694896a3eac1e3def83f218021a035fc32
Emanuel Martins de Lima <emanuelmartins1081@gmail.com>
2025-05-22 10:55:09 -0300

    Adição do exercicio 25 da Faccat

--- Faccat/Exercício 25 - Verificar Tipo de Saldo/js/script.js
+++ Faccat/Exercício 25 - Verificar Tipo de Saldo/js/script.js
@@ -1,4 +1,17 @@
 alert('Verificação do Tipo de Saldo');
 
-let numeroConta = parseInt(prompt('Digite o número da conta do cliente: '));
-let saldoAtual = parseFloat(prompt(''));
+let numeroConta = prompt('Digite o número da conta do cliente: ');
+let saldoAnterior = parseFloat(prompt('Digite o valor anterior do saldo do cliente:'));
+let debito = parseFloat(prompt('Digite o valor do débito do cliente: '));
+let credito = parseFloat(prompt('Digite o valor do crédito do cliente: '));
+
+let saldoAtual = saldoAnterior - debito + credito;
+
+let mensagem = '';
+if(saldoAtual >= 0){
+    mensagem = 'Saldo Positivo';
+} else {
+    mensagem = 'Saldo Negativo';
+}
+
+alert(`Informações da Conta\nNúmero da Conta: ${numeroConta}\nSaldo Anterior: R$ ${saldoAnterior.toFixed(2)}\nValor Debitado: R$ ${debito.toFixed(2)}\nValor Creditado: R$ ${credito.toFixed(2)}\nSaldo Atual: R$ ${saldoAtual.toFixed(2)}\n${mensagem}`);
